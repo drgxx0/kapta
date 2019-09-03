4de25c50d1cb3b8305109042231911db2654ca26
unknown <alejandroff91@gmail.com>
2019-09-03 00:35:32 -0500

    cambios

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kapta/precache-manifest.1b971126060bd98cf9861f221c2f4cd8.js"
+  "/kapta/precache-manifest.57e000847380914e792aa434b3b7a14a.js"
 );
 
 self.addEventListener('message', (event) => {
